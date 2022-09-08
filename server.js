12aca4fe3de64a0a72343b64bedf28f7af6b833a
Rob Webster <robwebster@mailbox.org>
2022-09-08 07:09:24 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+  console.log("Not in production environment, loading dotenv");
+  require("dotenv").config();
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
